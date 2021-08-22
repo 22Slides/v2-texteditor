3fc542726a9ec299efb918a4103bd1ed7d5a44f3
Bryan Buchanan <bryan@resen.co>
2021-08-22 07:58:27 -0700

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import postcss from 'rollup-plugin-postcss'
 import serve from 'rollup-plugin-serve'
-// import livereload from 'rollup-plugin-livereload'
 import { terser } from 'rollup-plugin-terser'
 
 const dev = process.env.ROLLUP_WATCH;
